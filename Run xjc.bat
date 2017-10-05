73f02f23f7fb91e2c0d20a586f7cc6aa11f2454b
Gagan R Ichake <Ichake_Gagan_R@cat.com>
2017-10-05 13:09:10 -0500

    Replaced with relative path

--- Run xjc.bat
+++ Run xjc.bat
@@ -1,7 +1,5 @@
 @echo off
-set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.6.0_25
-set PROJ_HOME=C:\Users\Gagan\Workspace\JavaPractice\JdbcPractice
 path=%JAVA_HOME%\bin;
-cd=%PROJ_HOME%\XSD
-xjc -d %PROJ_HOME%/src -p gagan.practice.jaxb.beans.generated CandidateSearchCriteria.xsd
+cd=XSD
+xjc -d ../src -p gagan.practice.jaxb.beans.generated CandidateSearchCriteria.xsd
 pause
